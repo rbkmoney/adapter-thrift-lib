aefdaf37e164331f0790f72f4671b906d4fcc156
avcherkasov <shnurok14@ya.ru>
2020-03-11 14:04:38 +0300

    More P2pAdapterVerification

--- damsel-utils/src/main/java/com/rbkmoney/java/damsel/utils/verification/P2pAdapterVerification.java
+++ damsel-utils/src/main/java/com/rbkmoney/java/damsel/utils/verification/P2pAdapterVerification.java
@@ -1,11 +1,16 @@
 package com.rbkmoney.java.damsel.utils.verification;
 
 import com.rbkmoney.damsel.p2p_adapter.CallbackResult;
+import com.rbkmoney.damsel.p2p_adapter.ProcessResult;
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
 
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class P2pAdapterVerification {
+
+    public static boolean isSuccess(ProcessResult processResult) {
+        return processResult.getIntent().getFinish().getStatus().isSetSuccess();
+    }
 
     public static boolean isSuccess(CallbackResult callbackResult) {
         return callbackResult.getIntent().getFinish().getStatus().isSetSuccess();
@@ -15,8 +20,16 @@ public class P2pAdapterVerification {
         return callbackResult.getIntent().isSetSleep();
     }
 
+    public static boolean isSleep(ProcessResult processResult) {
+        return processResult.getIntent().isSetSleep();
+    }
+
     public static boolean isFailure(CallbackResult callbackResult) {
         return callbackResult.getIntent().getFinish().getStatus().isSetFailure();
+    }
+
+    public static boolean isFailure(ProcessResult processResult) {
+        return processResult.getIntent().getFinish().getStatus().isSetFailure();
     }
 
 }
